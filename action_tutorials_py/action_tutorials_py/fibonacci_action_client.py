1452e7c47706f566157202c2fa2b6245769139e8
obensustam2 <obensustam2@gmail.com>
2025-03-22 13:09:30 +0100

    fibonacci action python upgrade

--- action_tutorials_py/action_tutorials_py/fibonacci_action_client.py
+++ action_tutorials_py/action_tutorials_py/fibonacci_action_client.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2019 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,58 +12,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from action_tutorials_interfaces.action import Fibonacci
+from example_interfaces.action import Fibonacci
 
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
 
 
-class FibonacciActionClient(Node):
+class MinimalActionClient(Node):
 
     def __init__(self):
-        super().__init__('fibonacci_action_client')
+        super().__init__('minimal_action_client')
         self._action_client = ActionClient(self, Fibonacci, 'fibonacci')
 
-    def send_goal(self, order):
-        goal_msg = Fibonacci.Goal()
-        goal_msg.order = order
+    def cancel_done(self, future):
+        cancel_response = future.result()
+        if len(cancel_response.goals_canceling) > 0:
+            self.get_logger().info('Goal successfully canceled')
+        else:
+            self.get_logger().info('Goal failed to cancel')
 
-        self._action_client.wait_for_server()
-
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
-
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
+        rclpy.shutdown()
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
-
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected :(')
             return
 
+        self._goal_handle = goal_handle
+
         self.get_logger().info('Goal accepted :)')
 
-        self._get_result_future = goal_handle.get_result_async()
+        # Start a 2 second timer
+        # self._timer = self.create_timer(2.0, self.timer_callback)
 
-        self._get_result_future.add_done_callback(self.get_result_callback)
+    def feedback_callback(self, feedback):
+        self.get_logger().info('Received feedback: {0}'.format(feedback.feedback.sequence))
 
-    def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result.sequence))
-        rclpy.shutdown()
+    def timer_callback(self):
+        self.get_logger().info('Canceling goal')
+        # Cancel the goal
+        future = self._goal_handle.cancel_goal_async()
+        future.add_done_callback(self.cancel_done)
 
-    def feedback_callback(self, feedback_msg):
-        feedback = feedback_msg.feedback
-        self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
+        # Cancel the timer
+        self._timer.cancel()
+
+    def send_goal(self):
+        self.get_logger().info('Waiting for action server...')
+        self._action_client.wait_for_server()
+
+        goal_msg = Fibonacci.Goal()
+        goal_msg.order = 10
+
+        self.get_logger().info('Sending goal request...')
+
+        self._send_goal_future = self._action_client.send_goal_async(
+            goal_msg,
+            feedback_callback=self.feedback_callback)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    action_client = FibonacciActionClient()
+    action_client = MinimalActionClient()
 
-    action_client.send_goal(10)
+    action_client.send_goal()
 
     rclpy.spin(action_client)
 
--- action_tutorials_py/action_tutorials_py/fibonacci_action_server.py
+++ action_tutorials_py/action_tutorials_py/fibonacci_action_server.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2019 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,51 +10,97 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.import time
+# limitations under the License.
 
 import time
 
-from action_tutorials_interfaces.action import Fibonacci
+from example_interfaces.action import Fibonacci
 
 import rclpy
-from rclpy.action import ActionServer
+from rclpy.action import ActionServer, CancelResponse, GoalResponse
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 
 
-class FibonacciActionServer(Node):
+class MinimalActionServer(Node):
 
     def __init__(self):
-        super().__init__('fibonacci_action_server')
-        self._action_server = ActionServer(self, Fibonacci, 'fibonacci', self.execute_callback)
+        super().__init__('minimal_action_server')
 
-    def execute_callback(self, goal_handle):
+        self._action_server = ActionServer(
+            self,
+            Fibonacci,
+            'fibonacci',
+            execute_callback=self.execute_callback,
+            callback_group=ReentrantCallbackGroup(),
+            goal_callback=self.goal_callback,
+            cancel_callback=self.cancel_callback)
+
+    def destroy(self):
+        self._action_server.destroy()
+        super().destroy_node()
+
+    def goal_callback(self, goal_request):
+        """Accept or reject a client request to begin an action."""
+        # This server allows multiple goals in parallel
+        self.get_logger().info('Received goal request')
+        return GoalResponse.ACCEPT
+
+    def cancel_callback(self, goal_handle):
+        """Accept or reject a client request to cancel an action."""
+        self.get_logger().info('Received cancel request')
+        return CancelResponse.ACCEPT
+
+    async def execute_callback(self, goal_handle):
+        """Execute a goal."""
         self.get_logger().info('Executing goal...')
 
+        # Append the seeds for the Fibonacci sequence
         feedback_msg = Fibonacci.Feedback()
-        feedback_msg.partial_sequence = [0, 1]
+        feedback_msg.sequence = [0, 1]
 
+        # Start executing the action
         for i in range(1, goal_handle.request.order):
-            feedback_msg.partial_sequence.append(feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i-1])
-            self.get_logger().info('Feedback: {0}'.format(feedback_msg.partial_sequence))
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                self.get_logger().info('Goal canceled')
+                return Fibonacci.Result()
+
+            # Update Fibonacci sequence
+            feedback_msg.sequence.append(feedback_msg.sequence[i] + feedback_msg.sequence[i-1])
+
+            self.get_logger().info('Publishing feedback: {0}'.format(feedback_msg.sequence))
+
+            # Publish the feedback
             goal_handle.publish_feedback(feedback_msg)
+
+            # Sleep for demonstration purposes
             time.sleep(1)
 
         goal_handle.succeed()
 
+        # Populate result message
         result = Fibonacci.Result()
-        result.sequence = feedback_msg.partial_sequence
+        result.sequence = feedback_msg.sequence
+
+        self.get_logger().info('Returning result: {0}'.format(result.sequence))
+
         return result
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    fibonacci_action_server = FibonacciActionServer()
+    minimal_action_server = MinimalActionServer()
 
-    try:
-        rclpy.spin(fibonacci_action_server)
-    except KeyboardInterrupt:
-        pass
+    # Use a MultiThreadedExecutor to enable processing goals concurrently
+    executor = MultiThreadedExecutor()
+
+    rclpy.spin(minimal_action_server, executor=executor)
+
+    minimal_action_server.destroy()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
